594bb0cd27a11e039ab5e3c8d79f3e4d4989d41c
GuiCarrilho <guicarrilho2001@gmail.com>
2024-05-24 22:29:14 -0300

    ajusting infra

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,13 +3,13 @@ variable "DOCKER_PASSWORD" {default = ""}
 variable "commit" {default = ""}
 
 locals{
-  app_name = "example_app"
+  app_name = "equipamento"
   app_port = 8080
   instace_type = "t2.micro"
   device_name = "/dev/sdf"
   vol_size = 20
   commit = var.commit
-  app_image_url = "fabriciobcv/es2-grupoc-vadebicicleta-base-example"
+  app_image_url = "fabriciobcv/es2-grupoc-vadebicicleta-equipamento"
   docker_username = var.DOCKER_USERNAME
   docker_password = var.DOCKER_PASSWORD
 }
