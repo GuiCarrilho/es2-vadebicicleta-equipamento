c847446787c55dc5cd912122f378f9768e6cbd9a
GuiCarrilho <guicarrilho2001@gmail.com>
2024-05-24 21:58:23 -0300

    add spring

---
+++ src/main/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/Es2VadebicicletaEquipamentoApplication.java
@@ -0,0 +1,13 @@
+package com.es2.vadebicicleta.es2_vadebicicleta_equipamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es2VadebicicletaEquipamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Es2VadebicicletaEquipamentoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/Es2VadebicicletaEquipamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.es2.vadebicicleta.es2_vadebicicleta_equipamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Es2VadebicicletaEquipamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
