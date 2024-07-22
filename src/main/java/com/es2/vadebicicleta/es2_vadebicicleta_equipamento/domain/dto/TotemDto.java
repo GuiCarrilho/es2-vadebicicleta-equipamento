bb7ea99bddafdde726eeeeb8616d41b4ad350e2f
GuiCarrilho <guicarrilho2001@gmail.com>
2024-07-22 18:48:30 -0300

    fixed minor issues

--- src/main/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/domain/dto/TotemDto.java
+++ src/main/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/domain/dto/TotemDto.java
@@ -1,9 +1,6 @@
 package com.es2.vadebicicleta.es2_vadebicicleta_equipamento.domain.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
--- src/main/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/exception/Erro.java
+++ src/main/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/exception/Erro.java
@@ -9,7 +9,8 @@ public class Erro {
     private String codigo;
     private String mensagem;
 
-    public Erro(String codigo, String mensagem){
+    public Erro(String codigo, String mensagem) {
         this.codigo = codigo;
         this.mensagem = mensagem;
+    }
 }
