52f02ca53f6c398cc4cb20621b817e9628d78085
Guilherme Carrilho <148422243+GuiCarrilho@users.noreply.github.com>
2024-07-22 01:35:34 -0300

    Update NotFoundException.java
    modified NotFoundException class

--- src/main/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/exception/NotFoundException.java
+++ src/main/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/exception/NotFoundException.java
@@ -1,13 +1,11 @@
 package com.es2.vadebicicleta.es2_vadebicicleta_equipamento.exception;
 
-import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
-@Getter
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends RuntimeException {
-    private final String codigo;
-
-    public NotFoundException(String codigo, String mensagem){
-        super(mensagem);
-        this.codigo = codigo;
+    public NotFoundException(String message) {
+        super(message);
     }
 }
