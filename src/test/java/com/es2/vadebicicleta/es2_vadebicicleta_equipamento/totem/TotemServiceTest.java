0a4c73a00d1dd916da33746f9d51a1c37212b195
Guilherme Carrilho <148422243+GuiCarrilho@users.noreply.github.com>
2024-07-23 04:36:06 -0300

    Update TotemServiceTest

--- src/test/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/totem/TotemServiceTest.java
+++ src/test/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/totem/TotemServiceTest.java
@@ -22,7 +22,7 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-class TotemRepositoryTest {
+class TotemServiceTest {
 
     @Mock
     private IdGenerator idGenerator;
