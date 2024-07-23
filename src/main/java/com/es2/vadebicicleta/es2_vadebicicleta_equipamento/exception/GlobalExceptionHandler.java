1693bd3bccec1f8ee72bfcf66b5c883998ce676d
Guilherme Carrilho <148422243+GuiCarrilho@users.noreply.github.com>
2024-07-23 13:36:50 -0300

    Update GlobalExceptionHandler

--- src/main/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/exception/GlobalExceptionHandler.java
+++ src/main/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
