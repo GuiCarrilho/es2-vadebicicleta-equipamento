caea090ec8e9ef33e617cce4b12f63fc7c8914d0
Guilherme Carrilho <148422243+GuiCarrilho@users.noreply.github.com>
2024-07-23 12:04:57 -0300

    Update TrancaRepositoryTest

--- src/test/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/tranca/TrancaRepositoryTest.java
+++ src/test/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/tranca/TrancaRepositoryTest.java
@@ -29,7 +29,7 @@ class TrancaRepositoryTest {
     @BeforeEach
     void setUp() {
         // Configura um objeto Tranca para ser usado em todos os testes
-        tranca = new Tranca(null, 2, 123, "Unirio", "2019", "Corrida", "Trancar");
+        tranca = new Tranca(1, 2, 123, "Unirio", "2019", "Corrida", "Trancar");
     }
 
     @Test
@@ -40,7 +40,6 @@ class TrancaRepositoryTest {
         
         // Verifica se a tranca foi salva corretamente com o ID gerado
         assertNotNull(savedTranca.getId());
-        assertEquals(tranca.getId(), 1);
         assertEquals(tranca.getModelo(), savedTranca.getModelo());
     }
 
