568e339da223f9ef14e6d767d5e0238606128ccc
Guilherme Carrilho <148422243+GuiCarrilho@users.noreply.github.com>
2024-07-23 12:21:24 -0300

    fixed sonar issues TotemRepositoryTest

--- src/test/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/totem/TotemRepositoryTest.java
+++ src/test/java/com/es2/vadebicicleta/es2_vadebicicleta_equipamento/totem/TotemRepositoryTest.java
@@ -40,7 +40,7 @@ class TotemRepositoryTest {
         
         // Verifica se a bicicleta foi salva corretamente com o ID gerado
         assertNotNull(savedTotem.getId());
-        assertEquals(savedTotem.getId(), 1);
+        assertEquals(1, savedTotem.getId());
         assertEquals(totem.getLocalizacao(), savedTotem.getLocalizacao());
     }
 
